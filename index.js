0347eccf35ba8ee690f869574602ce45d12f15a6
David Chima <d.chima@ticketline.co.uk>
2023-01-13 12:11:00 +0000

    delay sending messages until initialisation is complete

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 
 'use strict'
 const logClass= require('./log-classs');
-var log;
+let log;
+let initialised = false;
 
 module.exports.init = async function init (logServer, debug = false) {
     log = new logClass.log(logServer, debug);
     var result = await log.init();
+    initialised = true;
     return (result);
 }
 
 module.exports.logger = async function logger (message) {
+    let retries = 0;
+
+    while(!initialised && retries++ < 10) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+
+    if(!initialised)
+    {
+        try {
+            throw new Error('Connection to logger not initialised');
+        }
+        catch (e) {
+            console.log("Error writing to logger");
+            console.log(e.message);
+            return e.message;
+        }
+    }
+    
     try {
         var logResult = await log.sendLog(JSON.stringify(message));
         return logResult;
